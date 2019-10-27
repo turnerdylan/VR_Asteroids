43119519a1aa8b44b7ed2b26087bd27f2d07d6ea
Dylan Turner <dytu4626@colorado.edu>
2019-10-27 10:09:29 -0600

    UI changes

---
+++ VR_Asteroids/Assets/Scripts/UIController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public TextMesh lives;
+    public int numOfLives = 3;
+
+    public TextMesh timer;
+    float currentTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lives.text = "Lives: " + numOfLives;
+        timer.text = "Timer: " + currentTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        int seconds = (int)(currentTime % 60);
+        timer.text = "Timer: " + currentTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        numOfLives--;
+        lives.text = "Lives: " + numOfLives;
+    }
+}
